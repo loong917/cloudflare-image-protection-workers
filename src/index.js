c20c51e6136e288d8b9a1d199b7017b071d663e4
loong917 <loong.me@outlook.com>
2026-03-04 15:39:01 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,107 @@
+import { randomUUID } from 'node:crypto';
+import { extname } from 'node:path';
+import { generateSignedUrl, verifySignedUrl } from './utils/r2.js';
+
+const BASE_URL = 'https://loongzxl.com';
+
+export default {
+	async fetch(request, env) {
+		switch (request.method) {
+			case 'GET':
+				const url = new URL(request.url);
+				// Make sure you have the necessary query parameters.
+				const access_key_id = url.searchParams.get('key');
+				if (!access_key_id) {
+					return responseHTML('invalid key');
+				}
+				const timestamp = url.searchParams.get('timestamp');
+				if (!timestamp) {
+					return responseHTML('invalid timestamp');
+				}
+				const sign = url.searchParams.get('sign');
+				if (!sign) {
+					return responseHTML('invalid sign');
+				}
+				const access_key_secret = env.ACCESS_KEY_SECRET;
+				// 待签名数据
+				const body = url.pathname.slice(1);
+				// 验证签名
+				const check = await verifySignedUrl(access_key_secret, body, timestamp);
+				if (!check) {
+					return responseHTML('verify sign failed');
+				}
+				if (check.code != 1) {
+					return responseHTML(check.msg);
+				}
+				const object = await env.MY_BUCKET.get(key);
+				if (object === null) {
+					return responseHTML('Object Not Found', 404);
+				}
+				const headers = new Headers();
+				object.writeHttpMetadata(headers);
+				headers.set('etag', object.httpEtag);
+				return new Response(object.body, {
+					headers,
+				});
+			case 'POST':
+				try {
+					const formData = await request.formData();
+					// 读取文件
+					const file = formData.get('file');
+					// 处理文件（如果存在）
+					if (file instanceof File) {
+						const buffer = await file.arrayBuffer();
+						const finalKey = randomUUID() + extname(file.name);
+						await env.MY_BUCKET.put(finalKey, buffer);
+						// 生成签名url
+						const access_key_id = env.ACCESS_KEY_ID;
+						const access_key_secret = env.ACCESS_KEY_SECRET;
+						const signedUrl = await generateSignedUrl(access_key_id, access_key_secret, finalKey);
+						const result = {
+							data: `${BASE_URL}/${finalKey}?${signedUrl}`,
+						};
+						return new Response(JSON.stringify(result, null, 2), {
+							headers: { 'Content-Type': 'application/json' },
+						});
+					} else {
+						return new Response(
+							JSON.stringify(
+								{
+									error: 'Not a file',
+								},
+								null,
+								2,
+							),
+							{ headers: { 'Content-Type': 'application/json' }, status: 500 },
+						);
+					}
+				} catch (error) {
+					return new Response(
+						JSON.stringify(
+							{
+								error: error.message,
+							},
+							null,
+							2,
+						),
+						{ headers: { 'Content-Type': 'application/json' }, status: 500 },
+					);
+				}
+			default:
+				return responseHTML('Method Not Allowed', 405);
+		}
+	},
+};
+
+const responseHTML = (msg, status) => {
+	const html = `<!DOCTYPE html>
+    <body>
+      <p style="font-family: -apple-system-font, Arial, sans-serif; font-size: 17px; text-align: center;">${msg}</p>
+    </body>`;
+	return new Response(html, {
+		headers: {
+			'content-type': 'text/html;charset=UTF-8',
+		},
+		status: status ?? 403,
+	});
+};
---
+++ src/utils/r2.js
@@ -0,0 +1,28 @@
+// How long an token should be valid for, in seconds
+const EXPIRY = 10 * 1000;
+
+export async function generateSignedUrl(access_key_id, access_key_secret, body) {
+	// timestamp
+	const timestamp = new Date().getTime();
+	// md5(keysecret+body+timestamp)
+	const dataToSigned = `${access_key_secret}${body}${timestamp}`;
+	const md5Data = createHash('md5').update(dataToSigned).digest('hex');
+	const verifiedData = Buffer.from(md5Data).toString('base64');
+	return `key=${access_key_id}&timestamp=${timestamp}&sign=${verifiedData}`
+}
+
+export async function verifySignedUrl(access_key_secret, body, timestamp) {
+	const assertedTimestamp = Number(timestamp);
+	// Signed requests expire after five minute. Note that this value should depend on your specific use case
+	if (Date.now() > assertedTimestamp + EXPIRY) {
+		return { code: 403, msg: `URL expired at ${new Date(assertedTimestamp + EXPIRY)}` };
+	}
+	// md5(keysecret+body+timestamp)
+	const dataToSigned = `${access_key_secret}${body}${assertedTimestamp}`;
+	const md5Data = createHash('md5').update(dataToSigned).digest('hex');
+	const verifiedData = Buffer.from(md5Data).toString('base64');
+	if (verifiedData != sign) {
+		return { code: 403, msg: 'invalid sign' };
+	}
+	return { code: 1, msg: 'passed' };
+}
